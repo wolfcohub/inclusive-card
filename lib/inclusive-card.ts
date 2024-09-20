f9b2a4a6bd67ac48b102700067b0c04acb7bc299
Ted Flynn <ted@wolfco.us>
2024-09-20 15:19:37 -0500

    Make exclusions even dumber (better).

--- lib/inclusive-card.ts
+++ lib/inclusive-card.ts
@@ -5,7 +5,6 @@
 export default class InclusiveCard extends HTMLElement {
   private _clickStart: Date | undefined;
   private _exclusionSelector: string | null;
-  private _exclusions: NodeListOf<HTMLAnchorElement> | null;
   private _linkSelector: string | null;
   private _link: HTMLAnchorElement | null;
 
@@ -14,7 +13,6 @@ export default class InclusiveCard extends HTMLElement {
 
     this._clickStart = undefined;
     this._exclusionSelector = this.getAttribute('exclusions');
-    this._exclusions = null;
     this._link = null;
     this._linkSelector = this.getAttribute('link-target');
   }
@@ -28,10 +26,6 @@ export default class InclusiveCard extends HTMLElement {
       ? this.querySelector(this._linkSelector)
       : null;
 
-    this._exclusions = this._exclusionSelector
-      ? this.querySelectorAll(this._exclusionSelector)
-      : null;
-
     return this;
   }
 
@@ -42,23 +36,33 @@ export default class InclusiveCard extends HTMLElement {
       this.handleMouseUp(event);
     });
 
-    this._exclusions!.forEach(exclusion => {
-      exclusion.addEventListener('click', this.handleExclusionClick);
-    });
-
     return this;
   }
 
-  handleExclusionClick = (event: MouseEvent): void => {
-    event.stopPropagation();
-  };
+  handleMouseDown = (event: MouseEvent): void => {
+    const target = event.target as HTMLElement;
+    if (this._exclusionSelector) {
+      if (target.matches(this._exclusionSelector)) {
+        event.stopPropagation();
 
-  handleMouseDown = (): void => {
+        return;
+      }
+    }
+
     this._clickStart = new Date();
   };
 
   handleMouseUp = (event: MouseEvent) => {
     const upStamp = new Date();
+    const target = event.target as HTMLElement;
+    if (this._exclusionSelector) {
+      if (target.matches(this._exclusionSelector)) {
+        event.stopPropagation();
+
+        return;
+      }
+    }
+
     if (upStamp.getTime() - this._clickStart!.getTime() < 200) {
       // Avoid double firing click events
       if (this._link !== event.target) {
